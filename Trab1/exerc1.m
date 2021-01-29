ea5fed6d4e6c7c50e086c8f7bd707dc45994c48d
Alexandre Vitor <alexandre.braga@estudante.ufjf.br>
2021-01-29 19:08:14 -0300

    Adiciona ao git o trab1

---
+++ Trab1/exerc1.m
@@ -0,0 +1,59 @@
+#!/home/kuroneko/snap/octave -qf
+1;
+function analise(matriz)
+  
+  #a
+  load("-mat",matriz);
+  a = Problem.A
+  n = rows(a);
+  
+  #b
+  [L,U,P] = lu(a);
+  
+  #c
+  #clf;
+  #spy(a);
+  #spy(L);
+  #spy(U);
+  
+  #d
+  preench = 100 - nnz(a)/(nnz(L)+nnz(U)) * 100
+  printf("preench = %f\n", preench);
+  
+  #e
+  b = a * ones(n,1); 
+  x = a\b; #Método de Gauss;
+  
+  #f
+  difX = ones(n,1) - x;
+  distX = norm(difX, inf) / norm(ones(n,1), inf);
+  printf("distX = %f\n", distX);
+  
+  #g
+  difA = a - (P * L * U);
+  distA = norm(difA, inf) / norm(a, inf);
+  printf("distA = %f\n", distA);
+  
+  #h
+  difB = b - ((P * L * U) * x);
+  distB = norm(difB, inf) / norm(b, inf);
+  printf("distB = %f\n", distB);
+  
+  #i
+  r = norm(b - a * x, inf);
+  printf("r = %f\n", r);
+  
+  #j
+  K = cond(a);
+  K2 = norm(a, inf) * norm(inv(a), inf);
+  
+  save ultimaMatriz.text a b x distX distA distB r K K2;
+
+ endfunction
+
+addpath(pwd);
+args = argv();
+printf("matriz = %s\n", args{1});
+analise(args{1});
+printf("Fim programa");
+
