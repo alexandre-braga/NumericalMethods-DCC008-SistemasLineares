4b85536f9b8ea6c3eb9e369b38b382c2536b4d8a
Alexandre Vitor <alexandre.braga@estudante.ufjf.br>
2021-02-06 19:36:14 -0300

    ajeita matrizes 10k

--- Trab1/exerc1.m
+++ Trab1/exerc1.m
@@ -44,10 +44,10 @@ function analise(matriz)
   printf("r = %f\n", r);
   
   #j
-  K = cond(a);
+  #K = cond(a);
   #K2 = norm(a, inf) * norm(inv(a), inf);
   
-  save ultimaMatriz.text x distX distA distB r K b a;
+  save ultimaMatriz.text x distX distA distB r b a;
 
  endfunction
 
